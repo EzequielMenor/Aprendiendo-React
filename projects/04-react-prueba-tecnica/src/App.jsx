a2c351ce1c6241a85a791666e91531bdde17eea0
Ezequiel Menor <ezequielmenor@gmail.com>
2024-05-06 12:35:14 +0200

    Tests

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,30 +1,29 @@
-import { useEffect, useState } from "react"
 import './App.css'
-import { getRandomFact } from "./services/facts"
 import { useCatImage } from "./hooks/useCatImage"
-
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+import { useCatFact } from './hooks/useCatFact'
+import { Otro } from './Components/Otro'
 
 export function App() {
-  const [fact, setFact] = useState()
+  const { fact, refreshFact } = useCatFact()
   const { imageUrl } = useCatImage({ fact })
 
-  useEffect(() => {
-    getRandomFact().then(newFact => setFact(newFact))
-  }, [])
 
   const handleClick = async () => {
-    const newFact = await getRandomFact()
-    setFact(newFact)
+    refreshFact()
   }
 
   return (
     <main>
       <h1>Apps de gatos</h1>
-
       <button onClick={handleClick}>Get new fact</button>
+
       {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first trhee words for ${fact}`} />}
+      <section>
+
+        {imageUrl && <img src={imageUrl} alt={`Image extracted using the first trhee words for ${fact}`} />}
+
+        <Otro />
+      </section>
     </main>
   )
 }
